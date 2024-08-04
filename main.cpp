a0f04edd1f6612cb6dff1fc1eaea9536c4037905
Leksusmart <leksusmartmail@gmail.com>
2024-08-04 15:08:51 +0300

    Update 2.0 files

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "WelcomeWindow.h"
+#include "GameWindow.h"
+#include <Windows.h>
+static bool buf = FreeConsole();//Закрываем консоль, чтобы не мешала
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]//main будет запускаться в отдельном потоке данных
+int main(array<String^>^ args) {
+	srand(time(NULL));
+	Application::SetCompatibleTextRenderingDefault(false);//Необходима для коректрой обработки текста
+	Application::EnableVisualStyles();//Подключаем различные визуальные стили
+		
+	Сапёр2::WelcomeWindow Welcomeform;
+	Application::Run(% Welcomeform);
+}
